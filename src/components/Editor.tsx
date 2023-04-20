1304bef4f402ad93448c0a10ec53fbc88acc4f43
FrostMiKu <i@frostmiku.com>
2023-04-20 21:11:40 +0800

    fix bug

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,29 +3,20 @@ import {useRef,useEffect,useState} from "react";
 import Vditor from "vditor";
 
 interface EditorProps {
-  content: string;
-  setContent: (content: string) => void;
+  setVd: (vd: Vditor) => void;
 };
 
 const Editor = (props: EditorProps) => {
   const editorRef = useRef<HTMLDivElement>(null);
-  const [vd, setVd] = useState<Vditor>();
   useEffect(() => {
     if (editorRef.current === null) return;
     const vditor = new Vditor(editorRef.current as HTMLElement, {
       after: () => {
-        vditor.setValue(props.content);
-        setVd(vditor);
+        props.setVd(vditor);
       },
-      input(value) {
-        props.setContent(value);
-      },
-      "cache": {
-        "id": "vditorCache"
-      }
+      cache : {id : "vditorCache"},
     });
   }, []);
-  useEffect(() => {vd?.setValue(props.content)},[vd,props.content]);
   return <div ref={editorRef} className="vditor" />;
 };
 
