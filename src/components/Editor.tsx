f1a339617b31e00888e469bafca7e87b45f1eaa5
FrostMiKu <i@frostmiku.com>
2023-04-23 19:04:28 +0800

    update editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,16 @@
 import "vditor/dist/index.css";
-import {useRef,useEffect,useState} from "react";
+import { useRef, useEffect, useState } from "react";
 import Vditor from "vditor";
+import { TagType } from "../data/note";
+import { Button } from "antd";
+import Tags from "./Tags";
 
 interface EditorProps {
+  tags: TagType[];
+  content?: string;
+  setTags: (tags: TagType[]) => void;
   setVd: (vd: Vditor) => void;
+  onLoging: () => void;
 };
 
 const Editor = (props: EditorProps) => {
@@ -13,11 +20,23 @@ const Editor = (props: EditorProps) => {
     const vditor = new Vditor(editorRef.current as HTMLElement, {
       after: () => {
         props.setVd(vditor);
+        if (props.content) {
+          vditor.setValue(props.content);
+        }
       },
-      cache : {id : "vditorCache"},
+      cache: { id: "vditorCache" },
+      height: 'calc(100% - 3rem)',
     });
   }, []);
-  return <div ref={editorRef} className="vditor" />;
+  return (
+    <div className="h-3/4 p-8 shadow-md rounded-md bg-white">
+      <div ref={editorRef} className="vditor" />
+      <div className="flex justify-between items-center mt-4">
+        <Tags tags={props.tags} setTags={props.setTags} />
+        <Button type="primary" onClick={props.onLoging}>✍🏼 Loging!</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
